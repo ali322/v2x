54f1df0fd072aa4cdd1ce0b0d1a6c45004616260
alichen <ali322@gmail.com>
2018-09-26 17:27:40 +0800

    change to stack

--- lib/scene/index.dart
+++ lib/scene/index.dart
@@ -14,7 +14,6 @@ class IndexScene extends StatefulWidget{
 }
 
 class _IndexState extends State<IndexScene>{
-  final _pageController = new PageController(initialPage: 0);
   int _pageIndex = 0;
   final _pages = <Widget>[
     PersistTabview(child: TopicsScene()),
@@ -26,21 +25,20 @@ class _IndexState extends State<IndexScene>{
   @override
     Widget build(BuildContext context) {
       return Scaffold(
-        body: PageView(
-          controller: _pageController,
-          onPageChanged: (int i) {
-            if (i != _pageIndex) {
-              setState(() {
-                _pageIndex = i;
-              });
-            }
-          },
-          children: _pages,
+        body: Stack(
+        children: List.generate(_pages.length, (int i) {
+          return Offstage(
+              offstage: i != _pageIndex,
+              child: _pages[i]
+            );
+          }),
         ),
         bottomNavigationBar: CupertinoTabBar(
           currentIndex: _pageIndex,
           onTap: (int i) {
-            _pageController.jumpToPage(_pageIndex);
+            setState(() {
+              _pageIndex = i;
+            });
           },
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
